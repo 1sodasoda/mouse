16bf5733c31ec7276cba3eea70050df26049e5e7
Sam Hahn <153153sh@gmail.com>
2026-07-04 22:33:25 +0800

    Add README.org

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (python3.withPackages (ps: with ps; [
+            pynput
+          ]))
+          python3
+        ];
+      };
+    };
+}
